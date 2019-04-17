dfb654b3e8347455f3e317899161367d54ea2dc0
trycatchhorn <trycatchhorn@gmail.com>
2019-04-17 16:47:35 +0200

    Added main for testing

---
+++ main.py
@@ -0,0 +1,7 @@
+from lib.aws_config_reader.aws_config import AwsConfigurationReader
+
+if __name__ == "__main__":
+    
+    reader = AwsConfigurationReader()
+    print(reader.config)
+    print(reader.credentials)
