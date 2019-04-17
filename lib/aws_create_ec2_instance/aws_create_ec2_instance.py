1c64c5f22c05a4227b91579659f44b2cd9472c38
trycatchhorn <trycatchhorn@gmail.com>
2019-04-17 18:04:51 +0200

    Added stuff

--- lib/aws_create_ec2_instance/aws_create_ec2_instance.py
+++ lib/aws_create_ec2_instance/aws_create_ec2_instance.py
@@ -1,7 +1,10 @@
-import boto3
-from aws_config_reader.aws_config import AwsConfig
+#import boto3
+from lib.aws_config_reader.aws_config import AwsConfiguration
+from lib.aws_config_reader.aws_config import AwsConfigurationReader
 
 if __name__ == "__main__":
     print('hello')
-    reader = AwsConfig()
+    reader = AwsConfigurationReader()
+    print(reader.config)
+    print(reader.credentials)
 
